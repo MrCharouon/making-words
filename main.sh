a961596cf8d9bc0bcdfbdf6b9010d0fe50554547
ali <ali.khatami2009@gmail.com>
2022-09-08 02:54:58 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,3 +8,14 @@ do
 done
 cat en.srt | tr -cs A-Za-z '\n' | tr A-Z a-z | sort |  egrep '^.{11}' | uniq -c | sort -rn > 11-final-to-end.txt
 echo "Create file 11-final-to-end.txt"
+
+for j in {4..10}
+do
+    cat $j.txt >> 20.txt
+    echo "…………………………………" >> 20.txt
+    echo "Added $i.txt to 20.txt"
+    sleep 1
+done
+cat 11-final-to-end.txt >> 20.txt
+echo "Added 11-final-to-end.txt to 20.txt"
+echo "Done"
